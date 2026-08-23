650deb00d94752e4b3133324b4ae09ff6c68a58a
Lee Hao Ming <hming0228@gmail.com>
2026-08-23 17:02:29 +0800

    Adjust width of overall accuracy chart
    Increased the width of the overall accuracy chart from 440 to 650 pixels.

--- app.py
+++ app.py
@@ -605,7 +605,7 @@ with tab_compare:
         )
         col_chart, col_metrics = st.columns([3, 1])
         with col_chart:
-            show_exported_figure("fig1_overall_accuracy.png", "Overall accuracy chart", width=440)
+            show_exported_figure("fig1_overall_accuracy.png", "Overall accuracy chart", width=650)
         with col_metrics:
             for name in acc_model_names:
                 st.metric(name, f"{results['models'][name]['test_accuracy'] * 100:.2f}%")
